551dc85583218872aaf4466532c89018d4f7d156
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-12-20 11:19:27 -0700

    Updated Parsing Method

--- AddNum.java
+++ AddNum.java
@@ -10,33 +10,37 @@ public class AddNum
    String parsingMethod (String[] string)
    {
      String returnString = null;
-     int returnInt = null; //Input is small, thus int, not long
-     float returnFloat = null; //Input is small, thus float, not double
+     int returnInt; //Input is small, thus int, not long
+     Boolean boolInt = false; //Null is not possible in primatives
+     float returnFloat; //Input is small, thus float, not double
+     Boolean boolFloat = false;
      try
      {
-       returnInt = Integer.parseInt(string[0]);
+       returnInt = Integer.parseInt(string);
        System.out.println("Please restart the program and type digits");
+       boolInt = true;
      }
-     catch (NumberFormatException e)
+     catch (NumberFormatException f)
      {
        try
        {
          System.out.println("Let's see if you typed a decimal.");
-         returnFloat = Float.parseFloat(input[0]);
+         returnFloat = Float.parseFloat(string);
          System.out.println("Looks like you typed a decimal. \nPlease restart the program and type digits.");
+         boolFloat = true;
        }
-       catch (NumberFormatException e)
+       catch (NumberFormatException g)
        {
          System.out.println("Let's see if you mistyped digit");
-         returnString = string[0];
+         returnString = string;
          System.out.println("Looks like you mistyped a digit. \nPlease restart the program and type digits.");
        }
        //To Evaluate a Boolean, must use String Functions
        //Code here to be continued
-       if (returnInt != null) return "integer";
-       if (returnFloat != null) return "decimal";
+       if (boolInt == true) return "integer";
+       if (boolFloat == true) return "decimal";
        if (returnString != null) {
-         Boolean returnBool = Boolean.parseBoolean(string[0]);
+         Boolean returnBool = Boolean.parseBoolean(string);
          if (returnBool == true) return "boolean";
          return "spelling mistake";
        } else {
@@ -44,12 +48,13 @@ public class AddNum
          System.exit(2);
        }
 
-     return ; //Must be a String Variable returning into
+     return "ERROR"; //Must be a String Variable returning into
    }
 
-   public static void main(String args[]) //Exception not caught yet
+   public static void main(String[] args) //Exception not caught yet
    throws ArrayIndexOutOfBoundsException, NumberFormatException
    {
+     Parsing object = new Parsing();
      try
      {
        input[0] = args[0];
@@ -75,7 +80,8 @@ public class AddNum
      try
      {
        //Need to change this line
-       num[0] = parsingMethod(input[0]); //Memory of ARGS[0]
+       parseReturn[0] = object.parsingMethod(input[0]); //Memory of ARGS[0]
+       parseReturn[1] = object.parsingMethod(input[1]); //Memory of ARGS[1]
      }
      catch (NumberFormatException e) {
        System.out.println("You must enter digits \n" +
@@ -83,8 +89,9 @@ public class AddNum
      // Feedback is not specific, a general parsing would work better
      System.exit(1);
    }
-   System.out.println("Parsing an Integer with Try-Catch.");
-   System.out.println("From String " + input[0] + " to Integer " + num[0]);
+   System.out.println("Parsing an Entry with a Parsing Method.");
+   System.out.println("From String " + input[0] + " to Identifier String " + parseReturn[0]);
+   System.out.println("From String " + input[1] + " to Identifier String " + parseReturn[1]);
    System.out.println("Done.");
    System.exit(0);
  }
